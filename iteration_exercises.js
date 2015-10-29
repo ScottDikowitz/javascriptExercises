6c0f7cb81d60af0566cc8fa69bd3928f2d3236d4
ScottWilliam <ScottWilliam@appacademy.io>
2015-10-29 14:10:12 -0400

    Complete iteration exercises.

---
+++ iteration_exercises.js
@@ -0,0 +1,26 @@
+Array.prototype.bubble_sort = function () {
+  var dup = this.slice(0)
+
+  for (var x = dup.length - 1; x >= 0; x--) {
+    for (var y = 0; y < dup.length; y++) {
+      if (dup[y] > dup[y + 1]) {
+        temp = dup[y + 1];
+        dup[y + 1] = dup[y];
+        dup[y] = temp;
+      }
+    }
+  }
+
+  return dup
+}
+
+String.prototype.substrings = function() {
+  var sub_strs = [];
+  for (var x = 0; x < this.length; x++){
+    for (var y = (x + 1); y <= this.length; y++){
+      sub_strs.push(this.slice(x,y));
+
+    }
+  }
+  return sub_strs;
+};
